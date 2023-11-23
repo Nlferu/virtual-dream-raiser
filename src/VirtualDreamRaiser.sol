28c99b8798ed338ff75c006c6b5d558390d2eb92
Niferu <wargacki.patryq@gmail.com>
2023-11-23 20:54:32 +0100

    some functions added

--- src/VirtualDreamRaiser.sol
+++ src/VirtualDreamRaiser.sol
@@ -38,29 +38,32 @@ contract VirtualDreamRaiser is Ownable, ReentrancyGuard, KeeperCompatibleInterfa
      */
 
     /// @dev Variables
+    uint256 private s_dreamId;
     uint256 private immutable i_interval;
-    uint256 private immutable s_lastTimeStamp;
+    uint256 private immutable i_lastTimeStamp;
 
     /// @dev Structs
     struct Dream {
+        address idToCreator;
         uint256 idToTime;
         uint256 idToGoal;
-        uint256 idToAmount;
+        uint256 idToTotalGathered;
+        uint256 idToBalance;
         string idToDescription;
         bool idToStatus;
+        bool idToPromoted;
     }
 
     address[] private walletsWhiteList;
 
     /// @dev Mappings
     mapping(uint256 => Dream) private s_dreams;
-    mapping(address => uint256) private s_addressToAmountFunded;
 
     /// @dev Events
 
     constructor(uint256 interval, address owner) Ownable(owner) {
         i_interval = interval;
-        s_lastTimeStamp = block.timestamp;
+        i_lastTimeStamp = block.timestamp;
     }
 
     /// @notice Creating dream event, which will be gathering funds for dream realization
@@ -68,7 +71,23 @@ contract VirtualDreamRaiser is Ownable, ReentrancyGuard, KeeperCompatibleInterfa
     /// @param description Description of dream, which people are funding
     /// @param expiration Dream funds gathering period expressed in days
     /// @param organizatorWallet Address of wallet, which will be able to withdraw donated funds
-    function createDream(uint256 goal, string calldata description, uint256 expiration, address organizatorWallet) external {}
+    function createDream(uint256 goal, string calldata description, uint256 expiration, address organizatorWallet) external {
+        Dream storage dream = s_dreams[s_dreamId];
+
+        s_dreamId += 1;
+        dream.idToCreator = msg.sender;
+        dream.idToTime = expiration;
+        dream.idToGoal = goal;
+        dream.idToDescription = description;
+        dream.idToStatus = true;
+
+        for (uint wallets = 0; wallets < walletsWhiteList.length; wallets++) {
+            if (organizatorWallet == walletsWhiteList[wallets]) {
+                dream.idToPromoted = true;
+                break;
+            }
+        }
+    }
 
     /// @notice Function, which will be called by Chainlink Keepers automatically always when dream event expire
     /// @param dreamId Unique identifier of dream
@@ -76,8 +95,33 @@ contract VirtualDreamRaiser is Ownable, ReentrancyGuard, KeeperCompatibleInterfa
 
     /// @notice Function, which allow users to donate for certain dream
     /// @param dreamId Unique identifier of dream
+    function fundDream(uint256 dreamId) external payable {
+        Dream storage dream = s_dreams[dreamId];
+        dream.idToTotalGathered += msg.value;
+        dream.idToBalance += msg.value;
+    }
+
+    /// @notice Function, which allows creator of dream event to withdraw funds
+    /// @param dreamId Unique identifier of dream
     /// @param amount Amount to be funded for certain dream
-    function fundDream(uint256 dreamId, uint256 amount) external {}
+    function realizeDream(uint256 dreamId, uint256 amount) external {
+        Dream storage dream = s_dreams[dreamId];
+
+        if (dream.idToCreator == msg.sender) {
+            if (dream.idToBalance > 0 && dream.idToBalance >= amount) {
+                dream.idToBalance -= amount;
+                (bool success, ) = dream.idToCreator.call{value: amount}("");
+                require(success, "Transfer Failed!");
+            }
+        }
+    }
+
+    /// @notice Function, which will show calculated USD value of all gathered ETH based on Chainlink price feeds
+    function calculateApproximateUsdValue() internal {
+        /** @dev Chainlink Keepers should keep calling this once a day */
+    }
+
+    //////////////////////////////////// @notice Virtual Dream Raiser Functions ////////////////////////////////////
 
     /// @notice Function, which allow users to donate for VirtualDreamRaiser creators
     /// @param amount Amount to be funded for VirtualDreamRaiser creators
@@ -86,22 +130,14 @@ contract VirtualDreamRaiser is Ownable, ReentrancyGuard, KeeperCompatibleInterfa
     /// @notice Function, which allow VirtualDreamRaiser creators to witdraw their donates
     function withdrawDonates() external onlyOwner {}
 
-    /// @notice Function, which allows creator of dream event to withdraw funds
-    /// @param dreamId Unique identifier of dream
-    /// @param amount Amount to be funded for certain dream
-    function realizeDream(uint256 dreamId, uint256 amount) external {}
-
-    /// @notice Function, which will show calculated USD value of all gathered ETH based on Chainlink price feeds
-    function calculateApproximateUsdValue() internal {
-        /** @dev Chainlink Keepers should keep calling this once a day */
-    }
-
     function addToWhiteList(address organizationWallet) private onlyOwner {}
+
+    //////////////////////////////////// @notice Chainlink Keepers Functions ////////////////////////////////////
 
     /// @notice This is the function that the Chainlink Keeper nodes call to check if performing upkeep is needed
     /// @param upkeepNeeded returns true or false depending on x conditions
     function checkUpkeep(bytes memory /* checkData */) public view override returns (bool upkeepNeeded, bytes memory /* performData */) {
-        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool timePassed = ((block.timestamp - i_lastTimeStamp) > i_interval);
 
         upkeepNeeded = (timePassed);
 
@@ -110,4 +146,6 @@ contract VirtualDreamRaiser is Ownable, ReentrancyGuard, KeeperCompatibleInterfa
 
     /// @notice Once checkUpkeep() returns "true" this function is called to execute endDream() and calculateApproximateUsdValue() functions
     function performUpkeep(bytes calldata /* performData */) external override {}
+
+    //////////////////////////////////// @notice Getters ////////////////////////////////////
 }
